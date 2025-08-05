01effac335ae5395e2b210daf4fa867572a939f6
zzitan <zaydzitan20@gmail.com>
2025-08-05 10:00:45 +0100

    add points and window

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use macroquad::prelude::*;
+
+#[derive(Clone, Copy)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+fn chaikin_step(points: Vec<Point>) -> Vec<Point> {
+    // q = 3/4 * p1 + 1/4 * p2  (1/4 point)
+    // r = 1/4 * p1 + 3/4 * p2  (3/4 point)
+    // let res  = Vec::new();
+
+    points
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Chaikin".to_string(),
+        window_width: 800,
+        window_height: 600,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut points: Vec<Point> = vec![];
+
+    loop {
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let (m_x, m_y) = mouse_position();
+            points.push(Point { x: (m_x), y: (m_y) });
+        }
+        clear_background(BLACK);
+
+        for point in &points{
+            draw_circle(point.x, point.y, 3.0, WHITE);
+        }
+
+
+        next_frame().await;
+    }
+}
