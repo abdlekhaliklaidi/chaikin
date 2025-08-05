79e1e05faa94b215f1b3520ef198e3d41bca9f0b
0xZED <zerfyury11@gmail.com>
2025-08-05 20:14:13 +0100

    fix main and fix steps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use macroquad::prelude::*;
+use std::thread;
+use std::time::Duration;
 
-fn chaikin(points: &Vec<(f32,f32)>) -> Vec<(f32, f32)> {
+fn chaikin(points: &Vec<(f32, f32)>) -> Vec<(f32, f32)> {
     // q = 3/4 * p1 + 1/4 * p2  (1/4 point)
     // r = 1/4 * p1 + 3/4 * p2  (3/4 point)
     let mut res = Vec::new();
@@ -12,10 +14,8 @@ fn chaikin(points: &Vec<(f32,f32)>) -> Vec<(f32, f32)> {
         let new_qx = 0.75 * p1.0 + 0.25 * p2.0;
         let new_qy = 0.75 * p1.1 + 0.25 * p2.1;
 
-
         let new_rx = 0.25 * p1.0 + 0.75 * p2.0;
         let new_ry = 0.25 * p1.1 + 0.75 * p2.1;
-        
 
         // let new_r = Point {
         //     x: 0.25 * p1.x + 0.75 * p2.y,
@@ -40,7 +40,8 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut points= vec![];
+    let mut points = vec![];
+    let mut original = vec![];
 
     let mut is_start = false;
     let mut c = 0;
@@ -49,19 +50,24 @@ async fn main() {
         if is_mouse_button_pressed(MouseButton::Left) && !is_start {
             // let (m_x, m_y) = mouse_position();
             points.push(mouse_position());
+            original = points.clone();
         }
-
-        for point in &points {
+        for point in &original {
             draw_circle(point.0, point.1, 3.0, WHITE);
         }
+        // for point in &points {
+        //     draw_circle(point.0, point.1, 3.0, WHITE);
+        // }
 
-        if is_key_pressed(KeyCode::Enter) && points.len() >= 2 {
+        if is_key_pressed(KeyCode::Enter) && points.len() >= 2 && !is_start {
+            // Start the Chaikin algorithm
             is_start = true;
+            original = points.clone();
         }
 
         if is_start {
             for i in 0..points.len() - 1 {
-                c += 1;
+                // c += 1;
                 let p1 = points[i];
                 let p2 = points[i + 1];
 
@@ -70,6 +76,11 @@ async fn main() {
 
             if c <= 7 {
                 points = chaikin(&points);
+                thread::sleep(Duration::from_secs(1));
+                c += 1;
+            } else {
+                points = original.clone();
+                c = 0;
             }
         }
 
